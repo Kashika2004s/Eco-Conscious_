4dc39c8fd7d57c0f7fcb05db88a3b15ec48ce51f
Kashika2004s <kashikassdn28@gmail.com>
2025-02-17 20:00:55 +0530

    kk

--- Eco-Conscious/src/Components/Categories.jsx
+++ Eco-Conscious/src/Components/Categories.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Styles/Categories.css";
+import box1 from "../assets/box1.png";
+import box2 from "../assets/box2.png";
+import box3 from "../assets/box3.png";
+import box4 from "../assets/box4.png";
+import box5 from "../assets/box5.png";
 
 const Categories = () => {
   const navigate = useNavigate();
@@ -14,7 +19,7 @@ const Categories = () => {
         <div className="categoriesContainer">
           <div
             className="categoryBox firstCategoryBox"
-            style={{ backgroundImage: `url(/box1.png)` }} // Direct reference
+            style={{ backgroundImage: `url(${box1})` }} // Use direct path
           >
             <button
               className="button_categories"
@@ -26,7 +31,7 @@ const Categories = () => {
           <div className="categoryBox secondCategoryBox">
             <div
               className="secondBoxChild1"
-              style={{ backgroundImage: `url(/box2.png)` }} // Direct reference
+              style={{ backgroundImage: `url(${box2})` }} // Use direct path
             >
               <button
                 className="button1"
@@ -37,7 +42,7 @@ const Categories = () => {
             </div>
             <div
               className="secondBoxChild2"
-              style={{ backgroundImage: `url(/box3.png)` }} // Direct reference
+              style={{ backgroundImage: `url(${box3})` }} // Use direct path
             >
               <button
                 className="button1"
@@ -53,11 +58,11 @@ const Categories = () => {
           <div
             className="categoryBox secondCategoryBox1"
             id="categoryBox4"
-            style={{ backgroundImage: `url(/box4.png)` }} // Direct reference
+            style={{ backgroundImage: `url(${box4})` }} // Use direct path
           ></div>
           <div
             className="categoryBox firstCategoryBox1"
-            style={{ backgroundImage: `url(/box5.png)` }} // Direct reference
+            style={{ backgroundImage: `url(${box5})` }} // Use direct path
           >
             <button
               className="button_categories"
