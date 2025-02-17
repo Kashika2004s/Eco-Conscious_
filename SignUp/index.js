a49192b840c14045f0b0778e9bd5d58eb57d16ab
Kashika2004s <kashikassdn28@gmail.com>
2025-02-17 19:29:12 +0530

    kk

--- SignUp/index.js
+++ SignUp/index.js
@@ -31,7 +31,7 @@ const port = process.env.PORT || 3000;
 
 // MongoDB Connection using URI from .env file
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
